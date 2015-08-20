54c24015fd2c18a7a1acd590c4074d5c99bf382c
Daiqian Zhang <daiqian.zhang@addepar.com>
2015-08-20 01:48:45 -0400

    finished basic functionality

--- BluetoothUnlock/main.m
+++ BluetoothUnlock/main.m
@@ -7,11 +7,38 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <IOBluetooth/IOBluetooth.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+NSString *TARGET = @"Galaxy Nexus";
+
+int main(int argc, const char * argv[])
+{
+    IOReturn r;
+    IOBluetoothDevice *target = nil;
+    
+    NSArray *devices = [IOBluetoothDevice pairedDevices];
+    for(IOBluetoothDevice *device in devices){
+        if([TARGET isEqualToString:device.name]){
+            NSLog(@"Target device found : %@", TARGET);
+            target = device; break;
+        }
+    }
+    
+    
+    Boolean isConnected = false;
+    system("osascript /Users/daiqian.zhang/Work/UnixUtil/lock_unlock_scenario/outRange.scpt");
+    while (true) {
+        system("sleep 0.1");
+        r = [target openConnection];
+        if (r == kIOReturnSuccess && isConnected == false) {
+            system("osascript /Users/daiqian.zhang/Work/UnixUtil/lock_unlock_scenario/inRange.scpt");
+            isConnected = true; NSLog(@"Connected to : %@", TARGET);
+        } else if (r != kIOReturnSuccess && isConnected == true) {
+            system("osascript /Users/daiqian.zhang/Work/UnixUtil/lock_unlock_scenario/outRange.scpt");
+            isConnected = false; NSLog(@"Disconnected to : %@", TARGET);
+        } else {
+            continue;
+        }
     }
     return 0;
 }
